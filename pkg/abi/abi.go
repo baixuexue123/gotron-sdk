fdefba320c588e96d70439410c41f7dd6ef58244
baixue <baixue@wecash.net>
2024-12-05 17:57:04 +0800

    fix GetPaddedParam

--- pkg/abi/abi.go
+++ pkg/abi/abi.go
@@ -132,17 +132,25 @@ func GetPaddedParam(param []Param) ([]byte, error) {
 				if (ty.Elem.T == eABI.IntTy || ty.Elem.T == eABI.UintTy) &&
 					ty.Elem.Size > 64 {
 					tmp := make([]*big.Int, 0)
-					tmpSlice, ok := v.([]string)
+					tmpSlice, ok := v.([]any)
 					if !ok {
 						return nil, fmt.Errorf("unable to convert array of unints %+v", p)
 					}
-					for i := range tmpSlice {
+					strSlice := make([]string, len(tmpSlice))
+					for i, v := range tmpSlice {
+						str, ok := v.(string)
+						if !ok {
+							return nil, fmt.Errorf("unable to convert array of unints %+v", p)
+						}
+						strSlice[i] = str
+					}
+					for i := range strSlice {
 						var value *big.Int
 						// check for hex char
-						if strings.HasPrefix(tmpSlice[i], "0x") {
-							value, _ = new(big.Int).SetString(tmpSlice[i][2:], 16)
+						if strings.HasPrefix(strSlice[i], "0x") {
+							value, _ = new(big.Int).SetString(strSlice[i][2:], 16)
 						} else {
-							value, _ = new(big.Int).SetString(tmpSlice[i], 10)
+							value, _ = new(big.Int).SetString(strSlice[i], 10)
 						}
 						tmp = append(tmp, value)
 					}
